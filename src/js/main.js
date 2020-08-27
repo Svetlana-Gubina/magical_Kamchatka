f9408098793fc71fbf9020cff76df21580caefbf
Svetlana <gubinas83@gmail.com>
2020-08-27 18:44:05 +0300

    Fancybox, fix problem with closiing title

--- src/js/main.js
+++ src/js/main.js
@@ -37,30 +37,18 @@ $('[data-fancybox="gallery"]').fancybox({
   loop: true,
   infobar: false,
   arrows: false,
-  afterShow: function (instance, slide) {
-    // Tip: Each event passes useful information within the event object:
-    // Object containing references to interface elements
-    // (background, buttons, caption, etc)
-    // console.info( instance.$refs );
-    // Current slide options
-    // console.info(slide.opts);
-    // Clicked element
-    // console.info(slide.opts.$orig);
-    // Reference to DOM element of the slide
-    // console.info(slide.$slide);
-  },
   afterLoad: function (instance, current) {
     if (instance.group.length > 1 && current.$content) {
       current.$content.append(
         `<div class="slider__preview-flex">
-        <div class="slider__slide-title">
+        <div class="slider__slide-title" data-title>
           <p class="slider__slide-text">${
             current.opts.$orig[0].children[0].alt
           }</p>
           <p class="slider__slide-text">${
             current.opts.$orig[0].children[0].dataset.date
           }</p>
-          <button type="button" class="slider__slide-cancel"></button>
+          <button data-close type="button" class="slider__slide-cancel"></button>
         </div>
         <div class="slider__nav fancybox-navigation">
           <a data-fancybox-prev class="slider__previous" href="javascript:;">Предыдущий</a>
@@ -74,10 +62,11 @@ $('[data-fancybox="gallery"]').fancybox({
       </div>`
       );
     }
-    // const titleCancel = document.querySelector(".slider__slide-cancel");
-    // titleCancel.addEventListener("click", function (evt) {
-    //   evt.preventDefault();
-    //   this.closest(".slider__slide-title").classList.add("hidden");
-    // });
+
+    const btn = current.$slide.find(".slider__slide-cancel")[0];
+    btn.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      this.closest(".slider__slide-title").classList.add("hidden");
+    });
   },
 });
